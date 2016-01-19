ef1b0dc6fafc0d576389d04be9d432023abe326c
yaelschlotman <koshercouponmom@gmail.com>
2016-01-19 14:20:19 -0600

    create armory with weapons list, ammo list, way to calculate ammo and weapons inventory management.

--- TopSecretPlans/Armory.cs
+++ TopSecretPlans/Armory.cs
@@ -10,11 +10,59 @@ namespace TopSecretPlans
     {
         int numberOfWeapons;
         int ammo;
+        string[] weapons;
+        string[] guns;
+        string[] laserShooter;
+        string[] grenadeLaunchers;
+        string[] bombThrower;
+        string[] ammoType;
+        string[] grenades;
+        string[] bombs;
+        string[] superLaser;
+        string[] bullets;
+        int nWTotal;
+        int nWStock;
+        int nWTake;
+        int nWOut;
+        bool isWAvailable;
 
-        public Armory(int numberOfWeapons, int ammo) { }
+        public Armory(int numberOfWeapons, int ammo)
+        {
+             
+        }
+        public void weaponsList()
+        {
+            weapons = [guns, laserShooter, grenadeLaunchers, bombThrower];
+        }
+        public void ammoList()
+        {
+            ammoType = [bullets, superLaser, grenades, bombs];
+        }          
+        public void TotalWeapons()
+        {
+            nWTotal = nWStock + nWOut;
+            Console.WriteLine("The number of " + weapons + ":" + nWTotal);
+        }
+        public void WeaponsInStock()
+        {
+            nWStock = nWTotal - nWOut;
+            Console.WriteLine(weapons + " in stock:" + nWStock);
+        }
+        public void WeaponsOut() 
+        {
+            nWOut = nWTotal - nWStock;
+            Console.WriteLine(weapons + " out:" + nWOut);
+        }   
+        public void TakeWeapons()
+        {
+            Console.WriteLine("How many weapons do you want?");
+            string numberWeapons = Console.ReadLine();
+            int numberOfWeapons = int.Parse(numberWeapons);
+            
+        }        
+        public void TakeAmmo()
+        {
 
-        public void TakeWeappons() { }
-
-        public void TakeAmmo() { }
+        }
     }
 }
