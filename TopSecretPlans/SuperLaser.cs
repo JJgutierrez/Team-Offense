1e76b6e4683183c2e435780f61c8b0e65d450195
Nick Wilson <Nick Wilson>
2016-01-19 12:14:26 -0600

    created a recharge function for the Super Laser and added chargerPack member variable

--- TopSecretPlans/SuperLaser.cs
+++ TopSecretPlans/SuperLaser.cs
@@ -10,8 +10,28 @@ namespace TopSecretPlans
     {
         int laserPowerLevel;
         int laserDamage;
-        public SuperLaser(int laserPowerLevel, int laserDamage) { }
+        int chargerPack;
+         
+        public SuperLaser(int laserPowerLevel, int laserDamage, int chargerPack)
+        {
+            this.laserPowerLevel = laserPowerLevel;
+            this.laserDamage = laserDamage;
+            this.chargerPack = chargerPack;
+        }
 
-        public void FireLaser() { }
+        public void FireLaser()
+        {
+            laserPowerLevel -= 5;
+            Console.WriteLine("Pew, pew, pew!");
+            if (laserPowerLevel <= 0)
+            {
+                Recharge();
+            }
+        }
+        public void Recharge()
+        {
+            laserPowerLevel = chargerPack;
+            Console.WriteLine("The Super Laser is recharging");
+        }
     }
 }
